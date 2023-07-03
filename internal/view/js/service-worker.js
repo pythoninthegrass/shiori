277d2c89598bac915791bc56132eabba0a170a04
pythoninthegrass <4097471+pythoninthegrass@users.noreply.github.com>
2023-07-03 00:55:31 -0500

    PWA
    Incorporate https://github.com/go-shiori/shiori/pull/325 changes, copy assets to static res dir, add json mime type for manifest.json

---
+++ internal/view/js/service-worker.js
@@ -0,0 +1,103 @@
+// SOURCE: @geekgonecrazy https://github.com/go-shiori/shiori/pull/325
+// This is the service worker with the Advanced caching
+
+importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox-sw.js');
+
+const API_CACHE = "api";
+const BOOKMARK_CACHE = "bookmarks";
+const HTML_CACHE = "html";
+const JS_CACHE = "javascript";
+const STYLE_CACHE = "stylesheets";
+const IMAGE_CACHE = "images";
+const FONT_CACHE = "fonts";
+
+self.addEventListener("message", (event) => {
+  if (event.data && event.data.type === "SKIP_WAITING") {
+    self.skipWaiting();
+  }
+});
+
+workbox.routing.registerRoute(
+  ({ url, request, event }) => url.pathname === '/api/bookmarks',
+  new workbox.strategies.NetworkFirst({
+    cacheName: API_CACHE,
+    plugins: [
+      new workbox.expiration.ExpirationPlugin({
+        maxEntries: 50,
+      }),
+    ],
+  })
+);
+
+workbox.routing.registerRoute(
+  ({ url, event }) => event.request.destination === 'document' && url.pathname.startsWith('/bookmark'),
+  new workbox.strategies.NetworkFirst({
+    cacheName: BOOKMARK_CACHE,
+    plugins: [
+      new workbox.expiration.ExpirationPlugin({
+        maxEntries: 500,
+      }),
+    ],
+  })
+);
+
+workbox.routing.registerRoute(
+  ({ url, event }) => event.request.destination === 'document' && !url.pathname.startsWith('/bookmark'),
+  new workbox.strategies.NetworkFirst({
+    cacheName: HTML_CACHE,
+    plugins: [
+      new workbox.expiration.ExpirationPlugin({
+        maxEntries: 10,
+      }),
+    ],
+  })
+);
+
+
+workbox.routing.registerRoute(
+  ({ event }) => event.request.destination === 'script',
+  new workbox.strategies.StaleWhileRevalidate({
+    cacheName: JS_CACHE,
+    plugins: [
+      new workbox.expiration.ExpirationPlugin({
+        maxEntries: 15,
+      }),
+    ],
+  })
+);
+
+workbox.routing.registerRoute(
+  ({ event }) => event.request.destination === 'style',
+  new workbox.strategies.StaleWhileRevalidate({
+    cacheName: STYLE_CACHE,
+    plugins: [
+      new workbox.expiration.ExpirationPlugin({
+        maxEntries: 15,
+      }),
+    ],
+  })
+);
+
+workbox.routing.registerRoute(
+  ({ event }) => event.request.destination === 'image',
+  new workbox.strategies.StaleWhileRevalidate({
+    cacheName: IMAGE_CACHE,
+    plugins: [
+      new workbox.expiration.ExpirationPlugin({
+        maxEntries: 15,
+      }),
+    ],
+  })
+);
+
+workbox.routing.registerRoute(
+  ({ event }) => event.request.destination === 'font',
+  new workbox.strategies.StaleWhileRevalidate({
+    cacheName: FONT_CACHE,
+    plugins: [
+      new workbox.expiration.ExpirationPlugin({
+        maxEntries: 15,
+      }),
+    ],
+  })
+);
